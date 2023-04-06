e803d18b2deccc7037050adbbd30fa0cde010c0a
Nicooo67 <120011652+Nicooo67@users.noreply.github.com>
2023-04-06 13:57:17 +0100

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				lightWith: "#F1F1F1"
+			}
+		},
+	},
+	plugins: [],
+}
